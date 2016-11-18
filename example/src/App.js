3fa94e2f59ec9fe547a3802b801a704c014294a0
tngan <tonynwk919@gmail.com>
2016-11-18 21:26:26 +0800

    bump v1.0.0

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import ButterflyButton, { TB, LR, C } from '../../build';
+import ButterflyButton, { TB, LR, C } from 'react-butterfly-button';
 
 class App extends Component {
   render() {
